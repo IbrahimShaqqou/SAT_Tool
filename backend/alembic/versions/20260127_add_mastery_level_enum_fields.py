188acc1a9d785e5a51a762789a9ce10a166e04b0
Ibrahim Shaqqou <ibraheemshaqqou@gmail.com>
2026-01-28 00:36:05 -0500

    fixes databse migration

--- backend/alembic/versions/20260127_add_mastery_level_enum_fields.py
+++ backend/alembic/versions/20260127_add_mastery_level_enum_fields.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '20260127_mastery_levels'
-down_revision: Union[str, None] = '20260120_add_time_expired_to_assignments'
+down_revision: Union[str, None] = 'add_time_expired_assignments'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
